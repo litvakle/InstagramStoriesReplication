f1852ca27043709d59b874441d88b3c88e4359e4
Litvak Lev <sn.delui@gmail.com>
2022-10-27 08:26:40 +0500

    Initial Commit

---
+++ InstagramStoriesReplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramStoriesReplication
+//
+//  Created by Lev Litvak on 27.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramStoriesReplication/InstagramStoriesReplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoriesReplicationApp.swift
+//  InstagramStoriesReplication
+//
+//  Created by Lev Litvak on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoriesReplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
